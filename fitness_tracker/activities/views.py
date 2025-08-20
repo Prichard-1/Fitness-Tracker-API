c781f2621b8fb10828c91639ce2d978993af340c
Prichard Dube <genarodube8@gmail.com>
2025-08-20 08:02:45 +0200

    Add ActivityViewSet with user-specific queryset and authenticated permissions

--- fitness_tracker/activities/views.py
+++ fitness_tracker/activities/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+# activities/views.py
+from rest_framework import viewsets, permissions
+from .models import Activity
+from .serializers import ActivitySerializer
 
-# Create your views here.
+class ActivityViewSet(viewsets.ModelViewSet):
+    serializer_class = ActivitySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Activity.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
