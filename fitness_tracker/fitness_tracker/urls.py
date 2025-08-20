37624d89167e620609c828e84b77c17395ea240b
Prichard Dube <genarodube8@gmail.com>
2025-08-20 07:41:01 +0200

    Initial project setup

--- fitness_tracker/fitness_tracker/urls.py
+++ fitness_tracker/fitness_tracker/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
+    # JWT authentication
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # App routes
+    path('api/users/', include('users.urls')),        # for registration/login
+    path('api/activities/', include('activities.urls')),  # for fitness tracking
 ]
