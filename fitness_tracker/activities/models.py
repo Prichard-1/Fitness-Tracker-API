d098e15db3132e0c5489a8e0c5588f8bfb7cdfe4
Prichard Dube <genarodube8@gmail.com>
2025-08-20 07:57:43 +0200

    Add Activity model linked to User with activity details

--- fitness_tracker/activities/models.py
+++ fitness_tracker/activities/models.py
@@ -1,3 +1,22 @@
+# activities/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Activity(models.Model):
+    ACTIVITY_CHOICES = [
+        ('Running', 'Running'),
+        ('Cycling', 'Cycling'),
+        ('Weightlifting', 'Weightlifting'),
+        # Add more as needed
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
+    activity_type = models.CharField(max_length=20, choices=ACTIVITY_CHOICES)
+    duration = models.PositiveIntegerField()  # in minutes
+    distance = models.FloatField(blank=True, null=True)  # optional
+    calories_burned = models.FloatField(blank=True, null=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.activity_type} on {self.date}"
+
